603c1ac9a74697bf34ce2f950482d2b1fd423abe
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-12-23 13:34:44 +0500

    SELECT DISTINCT

--- SQL Statements/select.sql
+++ SQL Statements/select.sql
@@ -5,3 +5,11 @@ SELECT * FROM actor;
 SELECT first_name FROM actor;
 
 SELECT first_name, last_name FROM actor;
+
+-- SELECT DISTINCT column FROM table;
+
+SELECT DISTINCT release_year FROM film;
+
+SELECT DISTINCT(rental_rate) FROM film;
+
+SELECT DISTINCT rating FROM film;
