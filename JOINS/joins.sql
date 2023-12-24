856e32610c074f7c468448e4f4812cb2911dcea4
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-12-24 21:13:09 +0500

    Full Outer Joins

--- JOINS/joins.sql
+++ JOINS/joins.sql
@@ -23,3 +23,25 @@ FROM payment
 INNER JOIN customer
 ON payment.customer_id = customer.customer_id;
 
+-- Full Outer Joins
+
+-- SELECT * FROM TableA
+-- FULL OUTER JOIN TableB
+-- ON TableA.col_match = TableB.col_match;
+
+SELECT * FROM Registrations
+FULL OUTER JOIN Logins
+ON Registrations.name = Logins.name;
+
+SELECT * FROM TableA
+FULL OUTER JOIN TableB
+ON TableA.col_match = TableB.col_match
+WHERE TableA.id IS NULL OR
+TableB.id IS NULL;
+
+SELECT * FROM customer
+FULL OUTER JOIN payment
+ON customer.customer_id = payment.customer_id
+WHERE customer.customer_id IS NULL
+OR payment.payment_id IS NULL;
+
