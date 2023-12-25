4c1bfc5b7f369891b7b468049f159470cfaa8bb9
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-12-25 23:43:56 +0500

    SubQuery

--- Advanced SQL Commands/advanced_sql_commands.sql
+++ Advanced SQL Commands/advanced_sql_commands.sql
@@ -48,7 +48,40 @@ FROM customer
 SELECT LEFT(first_name,1) || last_name || '@gmail.com'
 FROM customer;
 
+-- SubQuery
+SELECT student,grade
+FROM test_scores
+WHERE grade > (SELECT AVG(grade) FROM test_scores);
 
+SELECT student,grade
+FROM test_scores
+WHERE student IN
+(SELECT student FROM honor_roll_table);
+
+SELECT column_name
+FROM table_name
+WHERE EXISTS
+(SELECT column_name FROM table_name WHERE condition);
+
+SELECT title,rental_rate
+FROM film
+WHERE rental_rate > (SELECT AVG(rental_rate) FROM film);
+
+SELECT film_id,title
+FROM film
+WHERE film_id IN
+(SELECT inventory.film_id
+FROM rental
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30')
+ORDER BY title;
+
+SELECT first_name,last_name
+FROM customer AS c
+WHERE EXISTS
+(SELECT * FROM payment as p
+where p.customer_id = c.customer_id
+AND amount > 11);
 
 
 
