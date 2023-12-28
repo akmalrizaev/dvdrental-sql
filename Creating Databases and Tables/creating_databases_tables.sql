e88c87d821395afe55665e209fdbe5386b47f013
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-12-28 11:00:35 +0500

    CREATE Table

--- Creating Databases and Tables/creating_databases_tables.sql
+++ Creating Databases and Tables/creating_databases_tables.sql
@@ -12,6 +12,43 @@
 -- - CHECK - Ensures that all values in a column satisfy certain consition
 -- - EXCLUSION - 
 
+-- CREATE Table
+-- CREATE TABLE table_name (
+--   column_name TYPE column_constraint,
+--   column_name TYPE column_constraint,
+--   table_constrait table_constraint
+--   ) INHERITS existing_table_name;
+
+CREATE TABLE players(
+player_id SERIAL PRIMARY KEY,
+age SMALLINT NOT NULL);
+
+CREATE TABLE account(
+	user_id SERIAL PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	password VARCHAR(50) NOT NULL,
+	email VARCHAR(250) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+	last_login TIMESTAMP
+);
+
+CREATE TABLE job(
+	job_id SERIAL PRIMARY KEY,
+	job_name VARCHAR(200) UNIQUE NOTNULL
+);
+
+CREATE TABLE account_job(
+	user_id INTEGER REFERENCES account(user_id),
+	job_id INTEGER REFERENCES job(job_id),
+	hire_date TIMESTAMP
+);
+
+
+
+
+
+
+
 
 
 
