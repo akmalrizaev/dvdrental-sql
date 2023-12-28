465156dc79073a63f7d5967214eeceeb17c29179
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-12-28 09:26:36 +0500

    Data types, Constraints

---
+++ Creating Databases and Tables/creating_databases_tables.sql
@@ -0,0 +1,17 @@
+-- Data Types
+-- Boolean - True or False
+-- Character - char, varchar, and text
+-- Numeric - integer and floating-point number
+-- Temporal - date, time, timestamp, and interval
+
+-- The most common constraints used:
+-- - NOTNULL - Ensures that a column cannot have NULL value 
+-- - UNIQUE - Ensures that all values in a column are different
+-- - PRIMARY key - Uniquely identifies each row/record in a database table 
+-- - FOREIGN key - Constarains data based on columns in other tables
+-- - CHECK - Ensures that all values in a column satisfy certain consition
+-- - EXCLUSION - 
+
+
+
+
